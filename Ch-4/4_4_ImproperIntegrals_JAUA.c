450791ad278bc58e78752eb4eed06822d764b418
Jonathan Urrutia <jaurrutia.95@ciencias.unam.mx>
2026-08-27 18:27:38 -0500

    midpoint convergido

--- Ch-4/4_4_ImproperIntegrals_JAUA.c
+++ Ch-4/4_4_ImproperIntegrals_JAUA.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
-#include<stdlib.h> // For the memory allocation
+#include <stdlib.h> // For the memory allocation
 #include<math.h> // fabs (float absolute) function
-#include<time.h> // To track time
 
 // Parameters for the integration convergence
 #define TOL 1.0e-5
@@ -13,9 +12,6 @@ float f( float x){
     return x * x;
 }
 
-float g( float x){
-    return pow(x,4) * log(x + sqrt( x * x + 1));
-}
 
 // Neville methodd to interpolate (Lagrange?)
 void polint(const double xa[], const double ya[], int n, double x, double *y, double *dy) {
@@ -88,6 +84,8 @@ void polint(const double xa[], const double ya[], int n, double x, double *y, do
     free(d);
 }
 
+
+
 /* C structures are like list's whose element can be of diferent data-tyepes.
  They cannot contain functions as elements, but they can be allocated with pointers. */
 struct integrate{
@@ -99,8 +97,8 @@ struct integrate{
 };
 
 /* Routine implementing the extended trapezoidal rule which has a structure as argument */
-void trapzoid( struct integrate *t){
-    float x, len, s, del;  // x: Evaluating point
+void midpoint( struct integrate *t){
+    float x, len, s, del, ddel;  // x: Evaluating point
                             // len: Number of grid points at current level
                             // s: partial sum of the current level
                             // del: step size at current level
@@ -110,27 +108,33 @@ void trapzoid( struct integrate *t){
                     // Increases the level for the partition grid when function is called
 
     if ( t -> n_step == 1){ // First evaluation: extrema values
-        ((*t).sum = (0.5) * ((*t).x_right - (*t).x_left) * ((*t).integrand( (*t).x_right ) + (*t).integrand( (*t).x_left) ));
+        (*t).sum = ((*t).x_right - (*t).x_left) * (*t).integrand( 0.5 * ( (*t).x_right + (*t).x_left)  ) ;
     } else {
         for(it = 1, j = 1; j < (*t).n_step - 1 ; j++)   // Loop to build  the grid by increasing j
-            it <<= 1;                                   // Each time all it is calles, it increases a power of 2 with a bit-wise operation
+            it *= 3;                                   // Each time all it is calles, it increases a power of 2 with a bit-wise operation
         len = it;
-        del = ((*t).x_right - (*t).x_left) / len;
+        del = ((*t).x_right - (*t).x_left) / (3.0 * len);
+        ddel = del + del;
         x = (*t).x_left + 0.5 * del;                    // Starts the grid by shiftinf it from the x_left  point
-        for(s = 0.0, j = 0; j < it; j++, x += del)      // Evaluation of the funtion at the points of the current grid (Level)
-            s += (*t).integrand(x); 
-        (*t).sum = 0.5 * ((*t).sum + del * s);          // adding values to our structure
+        s = 0.0;
+        for(j = 0; j < it; j++) {     // Evaluation of the funtion at the points of the current grid (Level)
+            s += (*t).integrand(x);
+            x += ddel;
+            s += (*t).integrand(x);
+            x += del;
+        }
+        (*t).sum = (*t).sum / 3.0 + del * s;          // adding values to our structure
     }
 
 }
 
 /* Routine with a tolerance to stop the calculations */
-void trapzoid_tol(struct integrate *t){
+void midpoint_tol(struct integrate *t){
     int j;
     float old = 0.0;    // previous step
 
     for(j=0; j <= JMAX; j++){
-        trapzoid( &(*t) );      // Performing 1 level of integration in the grid
+        midpoint( &(*t) );      // Performing 1 level of integration in the grid
         if( j >= 4 ){           // Forcing at least five iterations
             if( fabs( (*t).sum - old ) < TOL*fabs(old) || ((*t).sum == 0.0 && old == 0.0)){ // Checking convergence
                 return ;        // Void functions cannot return any value, thus this is how we exit the function
@@ -142,7 +146,7 @@ void trapzoid_tol(struct integrate *t){
 }
 
 /* Simpson with a tolerance to stop the calculations */
-void simpson_tol(struct integrate *t){
+void simp_midpoint_tol(struct integrate *t){
 
     float old_t = 0.0;    // previous step of trapeziod
     float old_s = 0.0;    // previous step of Simpson 
@@ -150,8 +154,8 @@ void simpson_tol(struct integrate *t){
     int j;
 
     for(j=0; j <= JMAX; j++){
-        trapzoid( &(*t) );      // Performing 1 level of integration in the grid
-        simp =  (4. * (*t).sum - old_t) / 3.;
+        midpoint( &(*t) );      // Performing 1 level of integration in the grid
+        simp =  (9. * (*t).sum - old_t) / 8.;
         if( j >= 4 ){           // Forcing at least five iterations
             if( fabs( simp - old_s ) < TOL*fabs(old_s) || (simp == 0.0 && old_s == 0.0)){ // Checking convergence
                 (*t).sum = simp;
@@ -176,9 +180,9 @@ void romberg_tol(struct integrate *t){
     int j;        //  iterator 
 
     h[0] = 1.0; 
-    for(j=0; j <= JMAX; j++){
-        trapzoid( &(*t) );      // Performing 1 level of integration in the grid
-        s[j] = t-> sum; 
+    for(j=1; j <= JMAX; j++){
+        midpoint( &(*t) );      // Performing 1 level of integration in the grid
+        s[j-1] = t-> sum; 
         if( j >= K ){           // Forcing at leat K integrations
             polint( &h[j-K], &s[j-K], K, 0.0, &ss, &dss); // Interpolation
             if (fabs(dss) <= TOL * fabs(ss)){
@@ -186,7 +190,7 @@ void romberg_tol(struct integrate *t){
                 return;
             }
         }
-        h[j+1] = 0.25 * h[j];
+        h[j] = h[j-1] /9.0 ;
         }
     printf("Too many steps in routine simpson_tol");
 }
@@ -208,7 +212,7 @@ int main(){
 
     for(i = 1; i <= 5; i++ ){
         printf("\tIteration n = %d -> %.10f\n", t.n_step, t.sum );
-        trapzoid( &t );
+        midpoint( &t );
     }
 
 // We can reboot the pointer by forcing the sum and step to zero
@@ -218,7 +222,7 @@ int main(){
     t.sum = 0;
 
     printf("\nTOL = %.2e, JMAX = %d\n", TOL, JMAX);
-    trapzoid_tol( &t );
+    midpoint_tol( &t );
     printf("\tIteration n = %d -> %.10f\n", t.n_step, t.sum );
 
 // Integration with Simpson
@@ -229,38 +233,14 @@ int main(){
     t.sum = 0;
 
     printf("\nSimpson\nTOL = %.2e, JMAX = %d\n", TOL, JMAX);
-    simpson_tol( &t );
+    simp_midpoint_tol( &t );
     printf("\tIteration n = %d -> %.10f\n", t.n_step, t.sum );
 
-// Complicated itegral
-    t.x_left = 0.0;
-    t.x_right = 2.0;
-    t.integrand = g;
-    t.n_step = 0;
-    t.sum = 0;
-
-    printf("\nChecking the complicated integral:\n");
-    trapzoid_tol( &t );
-    printf("\tIteration n = %d -> %.10f\n", t.n_step, t.sum );
-
-    t.n_step = 0;
-    t.sum = 0;
-    simpson_tol( &t );
-    printf("\tIteration n = %d -> %.10f\n", t.n_step, t.sum );
-
-    // 2. Record the starting clock tick
-    clock_t start_time = clock();
-    
+      
     t.n_step = 0;
     t.sum = 0;
     romberg_tol( &t );
     printf("\tIteration n = %d -> %.10f\n", t.n_step, t.sum );
-
-    // 3. Record the ending clock tick
-    clock_t end_time = clock();
-    // 4. Calculate the elapsed time in seconds
-    double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-    printf("Execution time: %f seconds\n", time_taken);
-
+    
     return 0;
 }
